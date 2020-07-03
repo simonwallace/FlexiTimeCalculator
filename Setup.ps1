98036e19090e62b884d2fd87a28a02d5f0cb0220
Simon Wallace <simonwallace@users.noreply.github.com>
2020-07-03 19:29:48 +0100

    Added Setup script

---
+++ Setup.ps1
@@ -0,0 +1,31 @@
+[CmdletBinding(DefaultParameterSetName = "Install")]
+param (
+
+    [Parameter(ParameterSetName = "Install")]
+    [switch]
+    $Install,
+
+    [Parameter(ParameterSetName = "Uninstall")]
+    [switch]
+    $Uninstall
+)
+
+$MODULE_NAME = "FlexiTimeCalculator"
+
+$installDirectory = $env:PSModulePath.Split(';')[0]
+
+if (Test-Path "$installDirectory/$MODULE_NAME") {
+
+    Remove-Item "$installDirectory/$MODULE_NAME" -Recurse
+}
+
+if ($Uninstall)  {
+
+    Write-Host "Removed $MODULE_NAME from '$installDirectory'."
+
+    return
+}
+
+Copy-Item "$PSScriptRoot/$MODULE_NAME" "$installDirectory/$MODULE_NAME" -Recurse -Force
+
+Write-Host "Installed $MODULE_NAME to '$installDirectory'."
